188519b17fdb64ece3f4a284baad074d4b329e05
Kiet <kietnguyen20101010@gmail.com>
2019-09-26 15:17:14 +0700

    toi ceil

--- Exercise04/Exercise04/Program.cs
+++ Exercise04/Exercise04/Program.cs
@@ -6,16 +6,19 @@ using System.Threading.Tasks;
 
 namespace Exercise04
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
 
             Xuat(5);
+            Console.WriteLine("gia tri cua so {0} mu {1} la {2} ",3,5,pow(3 , 5));
+            Console.WriteLine(" ABs of {0} la {1}", -5, ABS(-5));
+            Console.WriteLine("ceil lam tron cua sua {0} la {1}", 3.5, ceil(3.5f));          
         }
-        static bool IsOdd(int n)
+        public static bool IsOdd(int n)
         {
-            return n % 2 == 0 ? true : false;
+            return n % 2 == 1;
         }
         static bool IsPrime(int n)
         {
@@ -47,7 +50,48 @@ namespace Exercise04
                 Console.WriteLine(" day la SNT");
             }
             else Console.WriteLine("day khong phai la SNT");
+
+            Console.WriteLine("can bat 2 cua {0} la {1}", n , Square(n));
+            Console.WriteLine("can bat 3 cua {0} la {1}", n, Cube(n));
+
         }
- 
+        static int Square(int n)
+        {
+            int c;
+            return c = n*n;
+        }
+        static int Cube(int n)
+        {
+            int c;
+            return c = n * n * n;
+        }
+        static int pow(int x , int y)
+        {
+            int a =1;
+            for (int i=0; i<y ; i++)
+            {
+                a = a * x;
+            }
+            return a;
+        }
+        static int ABS(int x)
+        {
+            if (x > 0)
+                return x;
+            else
+                return -x;
+        }
+        static int ceil(float a)
+        { 
+            int x; //phan nguyen
+                x = (int)a;
+            int n;
+            if (a >= x + 0.5)
+            {
+                return n = x + 1;
+            }
+            else
+                return n = x;
+        }
     }
 }
