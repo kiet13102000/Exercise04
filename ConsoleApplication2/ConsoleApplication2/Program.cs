90d225f1ead983bcdf74aaa8de2f6b26dfe5eede
kiet nguyen <kietnguyen20101010@gmail.com>
2019-10-02 22:53:01 +0700

    vehinhdenH

--- ConsoleApplication2/ConsoleApplication2/Program.cs
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -10,9 +10,17 @@ namespace ConsoleApplication2
     {
         static void Main(string[] args)
         {
-            int[] arr = new int[10];
-            Input(arr);
-            hamgoi(arr);
+            //int[] arr = new int[10];
+            //Input(arr);
+           // hamgoi(arr);
+            vecot();
+            vehinhvuong();
+            vehinhvuongrongruoc();
+            vechuu();
+            vechuv();
+            vetamgiacnguoc();
+            vetamgiac();
+            veW();
         }
         public static void hamgoi(int[] arr )
         {
@@ -130,5 +138,320 @@ namespace ConsoleApplication2
              }
              return a;
         }
+        public static void vecot()
+        {
+            for(int i=0; i<4; i++)
+            {
+                Console.WriteLine("i");
+            }
+        }
+        public static void vehinhvuong()
+        {
+            for (int i=0; i< 4; i++)
+            {
+                for(int j=0; j<4;j++)
+                {
+                    Console.Write("* ");
+                    if (j == 3)
+                    {
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+        public static void vehinhvuongrongruoc()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write("o ");
+            }
+                Console.WriteLine();
+            for (int j = 0; j < 2; j++)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    if (i == 0 || i == 3)
+                    {
+                        Console.Write("o ");
+                    }
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            //Console.WriteLine();
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write("o ");
+            }
+        }
+
+
+        public static void vechuu()
+        {
+            Console.WriteLine();
+            for (int j = 0; j < 3; j++)
+            { 
+                for (int i = 0; i < 4; i++)
+                    {
+                        if (i == 0 || i == 3)
+                        {
+                             Console.Write("U");
+                        }
+                        
+                        else
+                        {
+                            Console.Write(" ");
+                        }
+                }
+                Console.WriteLine();
+            }
+                for (int i = 0; i < 4; i++)
+                {
+                      Console.Write("U");
+                }
+            Console.WriteLine();
+        }
+        public static void vechuv()
+        {
+            int n = 3;
+            for (int i = 0; i <= 2 * n; i++)
+            {
+                if (i == 0 || i == 2 * n)
+                {
+                    Console.Write("V");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+            //hang2
+            for (int i = 0; i <= 2 * n; i++)
+            {
+                if (i == 1 || i == 2 * n - 1)
+                {
+
+                    Console.Write("V");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+            //hang3
+            for (int i = 0; i <= 2 * n; i++)
+            {
+                if (i == 2 || i == 2 * n - 2)
+                {
+                    Console.Write("V");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+            //hang4
+            for (int i = 0; i <= 2 * n; i++)
+            {
+                if (i == n)
+                {
+                    Console.Write("V");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+        }
+        
+        public static void vetamgiacnguoc()
+            {
+           //hang1
+                int n = 3;
+                for (int i = 0; i <= 2 * n; i++)
+                {
+                Console.Write("V");
+                }
+                Console.WriteLine();
+                //hang2
+                for (int i = 0; i <= 2 * n; i++)
+                {
+                    if (i == 1 || i == 2 * n - 1)
+                    {
+
+                        Console.Write("V");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+                //hang3
+                for (int i = 0; i <= 2 * n; i++)
+                {
+                    if (i == 2 || i == 2 * n - 2)
+                    {
+                        Console.Write("V");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+                //hang4
+                for (int i = 0; i <= 2 * n; i++)
+                {
+                    if (i == n)
+                    {
+                        Console.Write("V");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+            Console.WriteLine();
+            Console.WriteLine("---------------------");
+        }
+        public static void vetamgiac()
+        {
+            int n = 3;
+            //hang1
+            for (int i = 0; i <= 2 * n; i++)
+            {
+                if (i == n)
+                {
+                    Console.Write("A");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+            //hang2
+            for (int i = 0; i <= 2 * n; i++)
+            {
+                if (i == 2 || i == 2 * n - 2)
+                {
+                    Console.Write("A");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+            //hang3
+            for (int i = 0; i <= 2 * n; i++)
+            {
+                if (i == 1 || i == 2 * n - 1)
+                {
+
+                    Console.Write("A");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+            //hang4
+
+            for (int i = 0; i <= 2 * n; i++)
+            {
+                Console.Write("A");
+            }
+            Console.WriteLine();
+            Console.WriteLine("---------------");
+        }
+        public static void veW()
+        {
+            int n = 7;
+           
+                for (int i = 1; i < n * 2; i++)
+                {
+                    if (i == n - (n - 1) || i == n || i == 2 * n - 1)
+                    {
+                        Console.Write("W");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+                for (int i = 1; i < n * 2; i++)
+                {
+                    if (i == n - (n - 2) || i == 2 * n - 2 || i == n - 1 || i == n + 1)
+                    {
+                        Console.Write("W");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+
+                }
+                Console.WriteLine();
+            for (int i = 1; i < n * 2; i++)
+            {
+                if (i == n - (n - 3) || i == 2 * n - 3 || i == n - 2 || i == n + 2)
+                {
+                    Console.Write("W");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+
+            }
+            Console.WriteLine();
+            for (int i = 1; i < n * 2; i++)
+            {
+                if (i == n - (n - 4) || i == 2 * n - 4 || i == n - 3 || i == n + 3)
+                {
+                    Console.Write("W");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+
+            }
+            Console.WriteLine();
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
+
